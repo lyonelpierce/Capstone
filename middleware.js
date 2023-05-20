7f3ac5387d38a99a759a7590ab276fce25071334
Lyonel Pierce <lyonel@live.com>
2023-05-19 22:19:24 -0400

    Middleware update

--- middleware.js
+++ middleware.js
@@ -1,9 +1,3 @@
-import { NextResponse } from "next/server";
-
 export { default } from "next-auth/middleware";
 
 export const config = { matcher: ["/profile", "/create-tattoo"] };
-
-export async function middleware(request) {
-  return NextResponse.redirect(new URL("/", request.url));
-}
