9e37b88b5e3c858bec118399d7b1e492b3a35ca7
Lyonel Pierce <lyonel@live.com>
2023-05-20 15:07:52 -0400

    Sweetalert2 added

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,6 +2,7 @@
 
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
+import Swal from "sweetalert2";
 
 import Profile from "@components/Profile";
 
@@ -21,19 +22,27 @@ const MyProfile = () => {
   }, [session?.user.id]);
 
   const handleDelete = async (post) => {
-    const hasConfirmed = confirm(
-      "Are you sure you want to delete this prompt?"
-    );
-    if (hasConfirmed) {
-      try {
-        await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
-        const filteredPosts = posts.filter((p) => p._id !== post._id);
+    const hasConfirmed = Swal.fire({
+      title: "Delete",
+      text: "Are you sure you want to delete this tattoo?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#22C55E",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+      reverseButtons: true,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        try {
+          fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
+          const filteredPosts = posts.filter((p) => p._id !== post._id);
 
-        setPosts(filteredPosts);
-      } catch (error) {
-        console.log(error);
+          setPosts(filteredPosts);
+        } catch (error) {
+          console.log(error);
+        }
       }
-    }
+    });
   };
 
   return (
