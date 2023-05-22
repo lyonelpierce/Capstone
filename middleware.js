04dffce22d74602b12fbbb0287d8a91ee2e62a47
Lyonel Pierce <lyonel@live.com>
2023-05-21 21:34:52 -0400

    Middleware redirect

--- middleware.js
+++ middleware.js
@@ -1,3 +1,15 @@
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export const config = { matcher: ["/profile", "/create-tattoo"] };
+export function middleware(request) {
+  const cookie = request.cookies.get("next-auth.session-token")?.value;
+  if (cookie) {
+    return NextResponse.next();
+  } else {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
+}
+
+export const config = {
+  matcher: ["/profile", "/create-tattoo"],
+};
