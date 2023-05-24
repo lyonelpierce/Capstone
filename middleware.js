5b58c690e35dbe07acba02cda076ec547e87988c
Lyonel Pierce <lyonel@live.com>
2023-05-24 10:10:08 -0400

    Middleware fix

--- middleware.js
+++ middleware.js
@@ -1,15 +1,15 @@
-// export { default } from "next-auth/middleware";
-import { NextResponse } from "next/server";
+export { default } from "next-auth/middleware";
+// import { NextResponse } from "next/server";
 
-export function middleware(request) {
-  const cookie = request.cookies.get("next-auth.session-token")?.value;
-  if (cookie) {
-    return NextResponse.next();
-  } else {
-    console.log(cookie);
-    return NextResponse.redirect(new URL("/", request.url));
-  }
-}
+// export function middleware(request) {
+//   const cookie = request.cookies.get("next-auth.session-token")?.value;
+//   if (cookie) {
+//     return NextResponse.next();
+//   } else {
+//     console.log(cookie);
+//     return NextResponse.redirect(new URL("/", request.url));
+//   }
+// }
 
 export const config = {
   matcher: ["/profile", "/create-tattoo"],
