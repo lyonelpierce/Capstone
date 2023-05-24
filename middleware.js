81106c245c3d53f2cd80ff71f8de408b684c4274
Lyonel Pierce <lyonel@live.com>
2023-05-24 10:44:42 -0400

    Middleware fix

--- middleware.js
+++ middleware.js
@@ -2,7 +2,7 @@
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
-  const cookie = request.cookies.get("next-auth.session-token")?.value;
+  const cookie = request.cookies.get("__Secure-next-auth.session-token")?.value;
   console.log(request.cookies);
   if (cookie) {
     return NextResponse.next();
