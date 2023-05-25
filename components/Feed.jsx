661547d0d8275d1447fec1f532116c59f45f5c69
Lyonel Pierce <lyonel@live.com>
2023-05-24 20:45:14 -0400

    cache

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -29,7 +29,7 @@ const Feed = () => {
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch("/api/prompt");
+      const response = await fetch("/api/prompt", { cache: "no-cache" });
 
       const data = await response.json();
       console.log(data);
